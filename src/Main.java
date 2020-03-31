dff6b8ccca0d8fe48f07f213ba90f7ad193f5fe9
Yuliya Mishina <julia.reve19@gmail.com>
2020-03-31 12:47:40 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int accountBalance = 2_000_000_000;
+        int amountTransfer = 500_000_000;
+        int total = accountBalance + amountTransfer;
+        System.out.println(total);
+    }
+}
